2d5426aca7a5e03d4c2c70ad70da6e20849be46a
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-15 22:27:44 -0600

    push for now

--- key_pair_management/terraform/main.tf
+++ key_pair_management/terraform/main.tf
@@ -1,5 +1,5 @@
 module "eks_cluster_creation" {
   source              = "git@github.com:ianforrest11/terraform_templates.git//aws/general_key_pair?ref=main"
   key_name            = var.prod_ec2_ssh_key_name
-  public_key          = var.prod_ec2_ssh_key
+  public_key          = var.prod_ec2_ssh_public_key
 }
