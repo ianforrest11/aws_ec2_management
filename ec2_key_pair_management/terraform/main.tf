48f8e454d5d630345df8d9603fa5f9e894522a6d
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-17 10:23:27 -0600

    push ec2 key pair creation

--- ec2_key_pair_management/terraform/main.tf
+++ ec2_key_pair_management/terraform/main.tf
@@ -1,5 +1,5 @@
 module "prod_eks_ec2_ssh_key_pair_secret" {
   source              = "git@github.com:ianforrest11/terraform_templates.git//aws//ec2_key_pair?ref=main"
   key_name            = var.prod_eks_ec2_ssh_key_name
-  public_key          = data.aws_secretsmanager_secret_version.prod_eks_ec2_ssh_key_public_version.secret_string
+  public_key          = trimspace(data.aws_secretsmanager_secret_version.prod_eks_ec2_ssh_key_public_version.secret_string)
 }
